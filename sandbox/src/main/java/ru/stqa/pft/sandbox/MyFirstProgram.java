0fe55a60e12c0fcaa43dfa8b74b98d2d60052902
panchukova.vc <panchukova@sibvaleo.com>
2021-03-15 00:04:58 +0700

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,31 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
  public static void main (String [] args) {
-   System.out.println("Hello,world!");
+   hello("world");
+   hello("User");
+   hello("Valentina");
+
+   double l =5; //обл видимости переменной это {}. значит переменная с именем l существует в функции  main
+   System.out.println("Площадь квадрата со стороной " + l + "=" + area(l) ); //функция с одним параметром l
+
+   double a = 4;
+   double b = 6;
+   System.out.println("Площадь прямоугольника со сторонами " + a  + "и" +  b  + "=" + area(a, b) ); // функция с двумя параметрами a b
+
  }
+
+  public static void hello (String somebody) {
+   System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len*len;
+  }
+
+  public static double area(double a, double b)  //для кажд переменной указывается ее тип. и перед именем функции указывается тип возвращаеого значения
+  {
+    return a * b;
+  }
+
 
 }
