2e8d341a3568a850dc3b94d9b4bd08cac6c020df
panchukova.vc <78275958+panchukovavc@users.noreply.github.com>
2021-10-15 01:05:04 +0300

    Задание №5. Реализовать двухуровневую архитектуру тестового набора
    По лекции 2.8.
    Создать для этих вспомогательных классов общий базовый класс BaseHelper, перенести в него низкоуровневые вспомогательные методы (заполнение отдельного поля, нажатие на кнопку или ссылку).

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -42,14 +41,7 @@ public class ApplicationManager {
     }
   }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
