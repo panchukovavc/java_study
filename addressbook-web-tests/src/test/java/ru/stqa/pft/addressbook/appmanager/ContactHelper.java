ac543b630f462b912c069ca2eea89f53581dc0d2
panchukova.vc <78275958+panchukovavc@users.noreply.github.com>
2021-10-28 13:36:35 +0300

    Ошибка: java.lang.NullPointerException (Лекция 4.3)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -42,4 +42,9 @@ public class ContactHelper extends HelperBase {
     return isElementPresent(By.name("selected[]"));
 
   }
+
+  public int getContactCount() {
+    return wd.findElements(By.name("selected[]")).size();
+  }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeleteTests extends TestBase {
   @Test
   public void testContactDelete (){
+    int before = app.getContactHelper().getContactCount();
     if(! app.getContactHelper().isThereAContact()){
       app.getContactHelper().createContact(new ContactData("name", "name2", "89231260083", "test@mail.ru"));
     }
@@ -13,6 +15,8 @@ public class ContactDeleteTests extends TestBase {
     app.getNavigationHelper().deleteSelectContact();
     app.getNavigationHelper().closeAlert();
     app.getNavigationHelper().gotoHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before -1);
 
   }
 }
