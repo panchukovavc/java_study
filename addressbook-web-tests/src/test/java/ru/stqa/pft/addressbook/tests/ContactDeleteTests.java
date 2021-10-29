9d0f23b496c47d1f7c8462754731783855338693
panchukova.vc <78275958+panchukovavc@users.noreply.github.com>
2021-10-30 01:40:13 +0300

    Лекция 4.6. Изменение теста ContactDeleteTests
    Сгенерированы методы toString, equals, hashCode в ContactData.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTests.java
@@ -20,5 +20,9 @@ public class ContactDeleteTests extends TestBase {
     List <ContactData> after =app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size() -1);
 
+    before.remove(before.size()-1);
+    Assert.assertEquals(before, after);
+
+
   }
 }
