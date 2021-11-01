6ca2b01af8ee96aab25f2f9350940964605769a4
panchukova.vc <78275958+panchukovavc@users.noreply.github.com>
2021-11-02 00:05:46 +0300

    Лекция 5.1. Используем 1 браузер для всех тестов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class GroupCreationTests extends TestBase {
 
 
-  @Test
+  @Test (enabled = false)
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List <GroupData> before =app.getGroupHelper().getGroupList();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -11,13 +11,13 @@ import java.util.List;
 
 public class GroupModificationTests extends TestBase{
 
-  @Test
+  @Test (enabled = false)
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
     if(!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1",null, null));
     }
-    List<GroupData> before =app.getGroupHelper().getGroupList();  //исправление ошибки, как в тесте удаления групп
+    List<GroupData> before =app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
